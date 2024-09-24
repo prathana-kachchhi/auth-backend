91f562e69a31fef54834e0ca690f93b6e40bf31e
prathana-kachchhi <prathanarejoice@gmail.com>
2024-09-24 14:40:24 +0530

    create a login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,17 @@
 import User from "../models/user.model.js";
 import joi from "joi";
 
+
+
+
+// SignupController
 const signupSchema = joi.object({
     username: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")).required(),
+    password: joi.string().pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")).required().messages({
+        "string.pattern.base": "Create a strong password",
+        "string.empty": "Password is required"
+    }),
 });
 
 export const signupUser = async (req, res) => {
@@ -13,7 +20,7 @@ export const signupUser = async (req, res) => {
 
         const { error } = signupSchema.validate(req.body);
         if (error) {
-            return res.status(400).json({ message: error.message });
+            return res.status(400).json({ message: error });
         }
 
         const user = await User.create({ username, email, password });
@@ -22,3 +29,38 @@ export const signupUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+// LoginController
+const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required().messages({
+        "string.empty": "Password is required"
+    }),
+});
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const { error } = loginSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const isMatch = await compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+
+        res.status(200).json({ message: "Login successful"});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
